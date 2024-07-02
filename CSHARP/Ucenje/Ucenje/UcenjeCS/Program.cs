a07f08e0db2ca639e9e3af434713d84fbab7d609
lukacuca0902 <lukacuca@gmail.com>
2024-07-02 16:03:10 +0200

    Csharp - ucenje

---
+++ CSHARP/Ucenje/Ucenje/UcenjeCS/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using UcenjeCS;
+
+E01UlazIzlaz.Izvedi();
+
+E02VarijableTipoviPodataka.Izvedi();
+
+//E01UlazIzlaz.Izvedi();
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Edunova!");
+//Console.WriteLine("Osijek");
+
+// Ovo je komentar jedna linija
+
+/* 
+ 
+ Komentar kroz više linija
+ 
+ */
+
+
+//Console.WriteLine("Zadnja linija čšćđžČŠĆĐŽ");
