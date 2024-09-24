66e06174cde564d35ecfe2df5b39202caf1a455b
lukacuca0902 <lukacuca@gmail.com>
2024-09-24 21:05:37 +0200

    Frontend hello world

--- SQL/frizerski_salon.sql
+++ SQL/frizerski_salon.sql
@@ -7,7 +7,7 @@ go
 use frizerski_salon;
 
 
-create table termin(
+create table termini(
 sifra int not null primary key identity(1,1),
 id_frizura int not null,
 id_djelatnik int not null,
@@ -15,21 +15,21 @@ id_korisnik int not null,
 datum datetime
 );
 
-create table frizura(
+create table frizure(
 id_frizura int not null primary key identity(1,1),
 naziv varchar(50) not null,
 cijena decimal(18,2) not null,
 trajanje int not null
 );
 
-create table djelatnik(
+create table djelatnici(
 id_djelatnik int not null primary key identity(1,1),
 ime varchar(40) not null,
 prezime varchar(50) not null,
 oib char(11)
 );
 
-create table korisnik(
+create table korisnici(
 id_korisnik int not null primary key identity(1,1),
 ime varchar(40) not null,
 prezime varchar(50) not null,
@@ -37,28 +37,28 @@ telefon varchar(20) not null
 );
 
 
-alter table termin add foreign key(id_frizura) references frizura(id_frizura);
+alter table termini add foreign key(id_frizura) references frizure(id_frizura);
 
-alter table termin add foreign key(id_djelatnik) references djelatnik (id_djelatnik);
+alter table termini add foreign key(id_djelatnik) references djelatnici (id_djelatnik);
 
-alter table termin add foreign key (id_korisnik) references korisnik (id_korisnik);
+alter table termini add foreign key (id_korisnik) references korisnici (id_korisnik);
 
-insert into korisnik(ime,prezime,telefon)  values 
+insert into korisnici(ime,prezime,telefon)  values 
 ('Martina','Mirković','0995814578'), 
 ('Alen','Ljubojević','0998758547'),
 ('Andrea','Srećković','099857845');
 
-insert into djelatnik(ime,prezime,oib)  values 
+insert into djelatnici(ime,prezime,oib)  values 
 ('Luka','Lukić','03745263781'), 
 ('Marinka','Markić','65738291564'),
 ('Ljerka','Ljerkić','26753482564');
 
-insert into frizura(naziv,cijena,trajanje)  values 
+insert into frizure(naziv,cijena,trajanje)  values 
 ('Bob frizura',50,120), 
 ('Pixie frizura',40,100),
 ('Visoki fade s bočnim razdjeljkom ',20,15);
 
-insert into termin (id_frizura,id_djelatnik,id_korisnik,datum) values
+insert into termini(id_frizura,id_djelatnik,id_korisnik,datum) values
 --Bob frizuru(1) će raditi Marinka(2) na Martini(1)
 (1,2,1,'2024-06-27 16:30'),
 --Pixie frizuru(2) će raditi Ljerka(3) na Andrei(3)
@@ -68,10 +68,10 @@ insert into termin (id_frizura,id_djelatnik,id_korisnik,datum) values
 
 
 
-select * from djelatnik;
-select * from korisnik;
-select * from frizura;
-select * from termin;
+select * from djelatnici;
+select * from korisnici;
+select * from frizure;
+select * from termini;
 
 
 
